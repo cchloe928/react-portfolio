572dc5df74a4b4165e5fe147a5279037cc095d42
chloegithub <miss_jihyun@hotmail.com>
2023-04-21 19:27:53 -0400

    contact update

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -1,20 +1,24 @@
-function Contact () {
-    return (
-        <div id="contact" className="contact">
-        <h1>Contact</h1>
-        <div className="contact-instagram">
-          <a href="https://www.instagram.com/wanderlust._.chloe/?hl=en">
-            <img src={require("./images/580b57fcd9996e24bc43c521.png")} alt="instagram" style={{width: '100%'}} />
-          </a>
-        </div>
-        <div className="contact-email">
-          <img src={require("./images/email.jpg")} alt="email" style={{width: '100%'}} />
-        </div>
-        <div className="contact-phone">
-          <img src={require("./images/call-or-text.png")} alt="phone" style={{width: '100%'}} />
-        </div>
+function Contact() {
+  return (
+    <div id="contact" className="contact">
+      <h1>Contact</h1>
+      <div className="contact-instagram">
+        <a href="https://www.instagram.com/wanderlust._.chloe/?hl=en">
+          <img src={require("./images/580b57fcd9996e24bc43c521.png")} alt="instagram" style={{ width: '100%' }} />
+        </a>
       </div>
-    )
+      <div className="contact-email">
+        <a href="mailto:chloechoii928@gmail.com">
+          <img src={require("./images/email.jpg")} alt="email" style={{ width: '100%' }} />
+        </a>
+      </div>
+      <div className="contact-phone">
+        <a href="tell:1234567890">
+          <img src={require("./images/call-or-text.png")} alt="phone" style={{ width: '100%' }} />
+        </a>
+      </div>
+    </div>
+  )
 }
 
 export default Contact
